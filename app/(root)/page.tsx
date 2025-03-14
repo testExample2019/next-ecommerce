236648dfa1259a90a749563cb386a40ad5ae4d10
eugene.martynenko <eugene.martynenko@dataart.com>
2025-03-14 13:09:41 +0200

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
       <ProductList
         data={products}
         title={"Newest Arrivals"}
-        limit={5}
+        limit={7}
       ></ProductList>
     </>
   );
